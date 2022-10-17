037589e6426533de7e1fecfffea66fcb20b5b8b3
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-10-17 16:40:10 +0600

    add to cart

--- src/Component/Shop/Food/DogFood.js
+++ src/Component/Shop/Food/DogFood.js
@@ -90,10 +90,18 @@ const DogFood = () => {
                   class="flex justify-between items-center"
                   bis_skin_checked="1"
                 >
-                  <span class="text-3xl font-bold text-gray-900 dark:text-white">
-                    $599
+                  <span class="text-3xl font-bold text-gray-900">
+                    {dogFood.price}
                   </span>
-                  Add to cart
+                  <span class="text-xl font-bold text-gray-900">
+                    {dogFood.weight}
+                  </span>
+                  <button type="button" class="w-10 h-10 text-base font-medium rounded-full text-white bg-green-500 hover:bg-green-700">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="mx-auto" fill="white" viewBox="0 0 1792 1792">
+                    <path d="M1600 736v192q0 40-28 68t-68 28h-416v416q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68v-416h-416q-40 0-68-28t-28-68v-192q0-40 28-68t68-28h416v-416q0-40 28-68t68-28h192q40 0 68 28t28 68v416h416q40 0 68 28t28 68z">
+                    </path>
+                </svg>
+            </button>
                 </div>
               </div>
             </div>
