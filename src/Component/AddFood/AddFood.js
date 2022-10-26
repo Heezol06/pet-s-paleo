c3627e63462c6bfc748be60424907989767938b0
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-10-26 18:41:51 +0600

    complete from

--- src/Component/AddFood/AddFood.js
+++ src/Component/AddFood/AddFood.js
@@ -1,17 +1,29 @@
 import React from "react";
 import bgImage from "../../asset/flat-lay-toys-with-food-bowl-fur-brush-dogs.jpg";
 const AddFood = () => {
+    const handleAddFood = (e) =>{
+e.preventDefault()
+const name = e.target.name.value;
+const type = e.target.type.value;
+const weight = e.target.weight.value;
+const url = e.target.url.value;
+const price = e.target.price.value;
+
+const food = {name, type, weight,url,price}
+
+    }
   return (
-    <div className="mt-20 py-20">
-      <h1>Add Some New Food.</h1>
+    <div className="">
       <div
-        className="hero min-h-screen bg-base-200"
+        className="hero min-h-screen bg-base-200 py-20"
         style={{ backgroundImage: `url(${bgImage})` }}
-      >
-        <form className="text-start">
+        >
+        <form className="grid grid-cols-3" onSubmit={handleAddFood}>
+          <div>
+          <h1 className="text-black text-5xl">Add Some New Food.</h1>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text">Food Name</span>
             </label>
             <input
               type="text"
@@ -23,7 +35,7 @@ const AddFood = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text">Animal Type</span>
             </label>
             <input
               type="text"
@@ -35,7 +47,7 @@ const AddFood = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text">Weight</span>
             </label>
             <input
               type="text"
@@ -47,7 +59,7 @@ const AddFood = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text">URL</span>
             </label>
             <input
               type="text"
@@ -59,7 +71,7 @@ const AddFood = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text">Price</span>
             </label>
             <input
               type="text"
@@ -68,6 +80,7 @@ const AddFood = () => {
               placeholder="Foods price in BDT"
               required
             />
+          </div>
           </div>
         </form>
       </div>
