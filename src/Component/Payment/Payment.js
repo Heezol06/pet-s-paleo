52bb716f4ea94f29a2023e5a8ed633ccedd8f3b4
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-11-04 22:47:33 +0600

    bug fix

--- src/Component/Payment/Payment.js
+++ src/Component/Payment/Payment.js
@@ -1,14 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
+// import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Payment = () => {
     const {id} = useParams()
-    const url = `http://localhost:5000/payment${id}`
-    const {data} =  useQuery(['payment', id], () => fetch(url).then(res=>res.json()))
+    const [product, setProduct] = useState("")
+    const url = `http://localhost:5000/payment/${id}`
+    useEffect(()=>{
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setProduct(data))
+    },[])
     return (
         <div className='my-20'>
-            <h1 className='text-3xl'>Please Pay For {id} </h1>
+            <h1 className='text-3xl'>Please Pay For {product?.name} </h1>
         </div>
     );
 };
